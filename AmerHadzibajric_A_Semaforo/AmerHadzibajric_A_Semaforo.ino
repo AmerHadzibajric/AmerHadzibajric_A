c942536ea215caa59df43ff50d821edf104760b1
Unknown <amer.amer9211@gmail.com>
2019-02-06 13:36:20 +0100

    Metodi finali

--- AmerHadzibajric_A_Semaforo/AmerHadzibajric_A_Semaforo.ino
+++ AmerHadzibajric_A_Semaforo/AmerHadzibajric_A_Semaforo.ino
@@ -1,6 +1,7 @@
-
 int numlampeggi = 0;
-
+int lamp = 0;
+int dverde = 0;
+int dgiallo = 0;
 void setup() {
   // put your setup code here, to run once:
   pinMode(13,OUTPUT); //verde
@@ -10,32 +11,32 @@ void setup() {
   pinMode(4,OUTPUT); //giallo
   pinMode(2,OUTPUT); //rosso
   RichiestaValori();
-  
- 
+  RichiestaTempoLamp();
+  RichiestaVerde();
+  RichiestaGiallo();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   digitalWrite (13,HIGH);
   digitalWrite(2,HIGH);
-  delay(100);
-  lampeggia(13, 500, numlampeggi);
+  delay(dverde);
+  lampeggia(13, lamp, numlampeggi);
   digitalWrite(13,LOW);
   digitalWrite(12,HIGH);
   digitalWrite(4,HIGH);
-  delay(1000);
+  delay(dgiallo);
   digitalWrite(12,LOW);
   digitalWrite(2,LOW);
   digitalWrite(4,LOW);
-  delay(500);
   digitalWrite(10,HIGH);
   digitalWrite(5,HIGH);
-  delay(1000);
-  lampeggia(5, 500, numlampeggi);
+  delay(dverde);
+  lampeggia(5, lamp, numlampeggi);
   digitalWrite(5,LOW);
   digitalWrite(4,HIGH);
   digitalWrite(12,HIGH);
-  delay(1000);
+  delay(dgiallo);
   digitalWrite(12,LOW);
   digitalWrite(4,LOW);
   digitalWrite(10,LOW);
@@ -56,4 +57,25 @@ void RichiestaValori()
   Serial.print ("Quanti lampeggi verdi vuoi?");
   while(Serial.available()== 0){};
   numlampeggi = Serial.readString().toInt();
+}
+void RichiestaTempoLamp()
+{
+  Serial.begin(9600);
+  Serial.println ("Quanto durano i lampeggi?");
+  while(Serial.available()== 0){};
+  lamp = Serial.readString().toInt();
+}
+void RichiestaVerde()
+{
+  Serial.begin(9600);
+  Serial.println ("Quanto dura il verde?");
+  while(Serial.available()== 0){};
+  dverde = Serial.readString().toInt();
+}
+void RichiestaGiallo()
+{
+  Serial.begin(9600);
+  Serial.println ("Quanto dura il giallo?");
+  while(Serial.available()== 0){};
+  dgiallo = Serial.readString().toInt();
 }
